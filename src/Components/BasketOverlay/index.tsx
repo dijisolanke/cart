c7230cbe613bcdc7354c3851c7f6755819590f6c
diji <diji@dijis-MacBook.local>
2024-11-22 10:04:07 +0000

    logic complete

--- src/Components/BasketOverlay/index.tsx
+++ src/Components/BasketOverlay/index.tsx
@@ -11,6 +11,43 @@ interface BasketOverlayProps {
 const BasketOverlay = ({ cart, isVisible, onClose }: BasketOverlayProps) => {
   if (!isVisible) return null;
 
+  const calculateDiscountForA = (cart: Item[]) => {
+    const discountItemA = cart.find((item) => item.id === "a"); // Apply discount to item with id "a"
+    if (discountItemA && discountItemA.quantity >= 3) {
+      return Math.floor(discountItemA.quantity / 3) * 20;
+    }
+    return 0;
+  };
+
+  // Function to calculate the discount for item B
+  const calculateDiscountForB = (cart: Item[]) => {
+    const discountItemB = cart.find((item) => item.id === "b"); // Apply discount to item with id "b"
+    if (discountItemB && discountItemB.quantity >= 2) {
+      return Math.floor(discountItemB.quantity / 2) * 15;
+    }
+    return 0;
+  };
+
+  const calculateTotalPrice = (cart: Item[]) => {
+    //sums the prices of all items in the cart, taking their quantities into account
+    const baseTotal = cart.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+
+    const discountA = calculateDiscountForA(cart);
+    const discountB = calculateDiscountForB(cart);
+    return baseTotal - discountA - discountB; //The final price is the baseTotal minus both discounts
+  };
+
+  const handleClearCart = () => {
+    localStorage.clear(); // Clear all local storage
+    window.location.reload(); // Refresh the page
+  };
+
+  const discountA = calculateDiscountForA(cart);
+  const discountB = calculateDiscountForB(cart);
+
   return (
     <div className="overlay">
       <div className="overlay-content">
@@ -34,9 +71,19 @@ const BasketOverlay = ({ cart, isVisible, onClose }: BasketOverlayProps) => {
           </ul>
         )}
         <p>
-          Total: £
-          {cart.reduce((total, item) => total + item.price * item.quantity, 0)}
+          Total: £{calculateTotalPrice(cart)}{" "}
+          <span className={cart.length ? "" : "hide-message"}>
+            (Includes discounts: £{discountA + discountB})
+            <br />
+            <br />
+            £20 off for every 3 "A"s,
+            <br />
+            £15 off for every 2 "B"s
+          </span>
         </p>
+        <button className="clear-cart-btn" onClick={handleClearCart}>
+          Clear Cart
+        </button>
       </div>
     </div>
   );
